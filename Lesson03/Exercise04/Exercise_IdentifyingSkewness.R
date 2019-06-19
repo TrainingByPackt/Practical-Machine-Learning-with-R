0dc1226819bd8c6245e838fc7e749c3c18af0b7a
brindhaj <brindha.priya@gmail.com>
2019-06-19 14:18:48 +0800

    Update Exercise_IdentifyingSkewness.R

--- Lesson03/Exercise04/Exercise_IdentifyingSkewness.R
+++ Lesson03/Exercise04/Exercise_IdentifyingSkewness.R
@@ -8,10 +8,14 @@ data(PimaIndiansDiabetes)
 #Printing the skewness of the columns 
 #Not skewed 
 skewness(PimaIndiansDiabetes$glucose)
-
-histogram(PimaIndiansDiabetes$glucose)
+histogram(PimaIndiansDiabetes$glucose,xlab="Glucose Concentration")
 
 #Highly skewed 
 skewness(PimaIndiansDiabetes$age)
+histogram(PimaIndiansDiabetes$age,xlab="Age(in years)")
 
-histogram(PimaIndiansDiabetes$age)
+#Log Transformation
+transformed_data=log(PimaIndiansDiabetes$age)
+
+#View histogram
+histogram(transformed_data,xlab="Transformed Age(In years)")
