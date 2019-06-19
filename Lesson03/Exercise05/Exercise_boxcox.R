c4990795fe7cec90b1636a4ff093bdc96557dbf9
brindhaj <brindha.priya@gmail.com>
2019-06-19 14:20:10 +0800

    Update Exercise_boxcox.R

--- Lesson03/Exercise05/Exercise_boxcox.R
+++ Lesson03/Exercise05/Exercise_boxcox.R
@@ -3,13 +3,17 @@ box_cox = boxcox(PimaIndiansDiabetes$age ~ 1, lambda = seq(-6,6,0.1))
 
 cox = data.frame(box_cox$x, box_cox$y)            
 
-cox2 = cox[with(cox, order(-cox$box_cox.y)),] # Order the new data frame by decreasing y
+# Order the new data frame by decreasing y
+cox2 = cox[with(cox, order(-cox$box_cox.y)),] 
 
-cox2[1,]                                  # Display the lambda with the greatest
+# Display the lambda with the greatest      
+cox2[1,]                                  
 
-lambda = cox2[1, "box_cox.x"]                 # Extract that lambda
+# Extract that lambda
+lambda = cox2[1, "box_cox.x"]                 
 
-transformed_box_cox = (PimaIndiansDiabetes$age ^ lambda - 1)/lambda   # Transform the original data
+# Transform the original data
+transformed_box_cox = (PimaIndiansDiabetes$age ^ lambda - 1)/lambda   
 
 #View histogram
 histogram(transformed_box_cox)
