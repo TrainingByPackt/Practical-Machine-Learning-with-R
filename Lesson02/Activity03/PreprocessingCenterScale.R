cc33644ad0ebff4ae837fa6393598e0d6f538843
steffimonteiro <steffim@packt.com>
2019-06-19 11:59:54 +0530

    Update PreprocessingCenterScale.R

--- Lesson02/Activity03/PreprocessingCenterScale.R
+++ Lesson02/Activity03/PreprocessingCenterScale.R
@@ -2,18 +2,23 @@ library(caret)
 library(mlbench)
 # load the dataset PimaIndiansDiabetes
 data(PimaIndiansDiabetes)
-
+library(dplyr)
 # view data
-summary(PimaIndiansDiabetes [,1:2])
+summary(PimaIndiansDiabetes)
+glimpse(PimaIndiansDiabetes)
 
 # perform center operation to the dataset
-params <- preProcess(PimaIndiansDiabetes[,1:2], 
-                     method=c("center","scale"))
+params <- preProcess(PimaIndiansDiabetes, 
+                     method=c("center","scale","pca"))
 
 print(params)
 
 # transform the dataset using the parameters
-new_dataset <- predict(params, PimaIndiansDiabetes[,1:2])
-
+new_dataset <- predict(params, PimaIndiansDiabetes)
+plot(PimaIndiansDiabetes[,1],PimaIndiansDiabetes[,2],col= c("red","blue"))
+plot(new_dataset[,1], new_dataset[,2], col = c("red","blue"))
 # summarize the transformed dataset
+head(new_dataset)
 summary(new_dataset)
+PimaIndiansDiabetes[,1:2]
+glimpse(new_dataset)
