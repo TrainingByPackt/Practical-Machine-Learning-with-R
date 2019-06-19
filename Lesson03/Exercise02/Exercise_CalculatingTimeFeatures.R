66cd1a67e76a84bdb19d23dc949ac5cb38f7a25a
brindhaj <brindha.priya@gmail.com>
2019-06-19 14:12:30 +0800

    Update Exercise_CalculatingTimeFeatures.R

--- Lesson03/Exercise02/Exercise_CalculatingTimeFeatures.R
+++ Lesson03/Exercise02/Exercise_CalculatingTimeFeatures.R
@@ -1,9 +1,11 @@
+##Install the libraries if it is not present
+#install.packages("lubridate",type="win.binary")
+
 ## Time Features
 library(lubridate)
 
 #Hour of Day
 hour=formatted_date$hour
-hour=hour(formatted_date)
 
 #Extract Minute
 min=minute(formatted_date)
