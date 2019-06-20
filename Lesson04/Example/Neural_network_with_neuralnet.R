d2ab84b16250213712adf0833067ddb9a1e26b6d
Ludvig Renbo Olsen <mail@ludvigolsen.dk>
2019-06-20 21:09:09 +0200

    Separating the examples

--- Lesson04/Example/Neural_network_with_neuralnet.R
+++ Lesson04/Example/Neural_network_with_neuralnet.R
@@ -1,14 +1,19 @@
+# Using Duration and Amount as predictors
+
 # Attaching neuralnet
 library(neuralnet)
 # Set seed for reproducibility and easier comparison
 set.seed(1)
 # Classifying if class is "Good"
 nn1 <- neuralnet(Class == "Good" ~ Duration + Amount,
-  train_set, linear.output = FALSE)
-plot(nn1, rep="best", fontsize = 10)
+                 train_set, linear.output = FALSE)
+plot(nn1, rep = "best", fontsize = 10)
 train_error <- nn1$result.matrix[1]
 train_error
 ## [1] 60.35553
+
+# ----------------------------------------------------------------
+# Using column 11 to 20 as predictors
 
 # Attach packages
 library(neuralnet)
@@ -20,7 +25,7 @@ set.seed(1)
 # Notice that we choose the predictors by subsetting the data frame
 # and use every column as predictor with the dot "~."
 nn2 <- neuralnet(Class == "Good" ~ ., train_set[, c(9, 11:20)],
-  linear.output = FALSE)
+                 linear.output = FALSE)
 plotnet(nn2, var_labs = FALSE)
 
 train_error <- nn2$result.matrix[1]
